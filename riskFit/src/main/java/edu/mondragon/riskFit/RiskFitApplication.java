aed70678e86dab19c144c217e5bb3c3390820f26
lidealvarez 2 <lide.alvarez2@alumni.mondragon.edu>
2025-01-13 15:08:34 +0100

    Spring boot aplikazioa gehitu

---
+++ riskFit/src/main/java/edu/mondragon/riskFit/RiskFitApplication.java
@@ -0,0 +1,13 @@
+package edu.mondragon.riskFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskFitApplication.class, args);
+	}
+
+}
---
+++ riskFit/src/test/java/edu/mondragon/riskFit/RiskFitApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mondragon.riskFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
