c40474c790dd754119b128a03c6a191a4a7d1371
lidealvarez 2 <lide.alvarez2@alumni.mondragon.edu>
2025-01-23 18:16:02 +0100

    coverage igotzen

--- src/test/java/edu/mondragon/riskFit/RiskFormTest2.java
+++ src/test/java/edu/mondragon/riskFit/RiskFormTest2.java
@@ -9,52 +9,56 @@ import edu.mondragon.riskFit.Model.RiskFormModel2;
 public class RiskFormTest2 {
 
     @Test
-    public void testConstructorAndGetters() {
-        RiskFormModel2 riskFormModel2 = new RiskFormModel2(25, 70.5, 180.2, 2, 8, 3);
+    public void testGetSetPlayerAge() {
+        RiskFormModel2 riskFormModel2 = new RiskFormModel2(25, 0, 0, 0, 0, 0);
 
-        assertEquals(25, riskFormModel2.getPlayer_Age());
-        assertEquals(70.5, riskFormModel2.getPlayer_Weight());
-        assertEquals(180.2, riskFormModel2.getPlayer_Height());
-        assertEquals(2, riskFormModel2.getPrevious_Injuries());
-        assertEquals(8, riskFormModel2.getTraining_Intensity());
-        assertEquals(3, riskFormModel2.getRecovery_Time());
+        int expectedAge = 25;
+        riskFormModel2.setPlayer_Age(expectedAge);
+        assertEquals(expectedAge, riskFormModel2.getPlayer_Age(), "La edad del jugador no se establece correctamente");
     }
 
     @Test
-    public void testSetters() {
-        RiskFormModel2 riskFormModel2 = new RiskFormModel2(25, 70.5, 180.2, 2, 8, 3);
+    public void testGetSetPlayerWeight() {
+        RiskFormModel2 riskFormModel2 = new RiskFormModel2(0, 70.5, 0, 0, 0, 0);
 
-        riskFormModel2.setPlayer_Age(30);
-        riskFormModel2.setPlayer_Weight(75.0);
-        riskFormModel2.setPlayer_Height(185.0);
-        riskFormModel2.setPrevious_Injuries(3);
-        riskFormModel2.setTraining_Intensity(9);
-        riskFormModel2.setRecovery_Time(4);
-
-        assertEquals(30, riskFormModel2.getPlayer_Age());
-        assertEquals(75.0, riskFormModel2.getPlayer_Weight());
-        assertEquals(185.0, riskFormModel2.getPlayer_Height());
-        assertEquals(3, riskFormModel2.getPrevious_Injuries());
-        assertEquals(9, riskFormModel2.getTraining_Intensity());
-        assertEquals(4, riskFormModel2.getRecovery_Time());
+        double expectedWeight = 70.5;
+        riskFormModel2.setPlayer_Weight(expectedWeight);
+        assertEquals(expectedWeight, riskFormModel2.getPlayer_Weight(), "El peso del jugador no se establece correctamente");
     }
-    
+
     @Test
-    public void testSettersAndGettersTogether() {
-        RiskFormModel2 riskFormModel2 = new RiskFormModel2(25, 70.5, 180.2, 2, 8, 3);
+    public void testGetSetPlayerHeight() {
+        RiskFormModel2 riskFormModel2 = new RiskFormModel2(0, 0, 180.2, 0, 0, 0);
 
-        riskFormModel2.setPlayer_Age(30);
-        riskFormModel2.setPlayer_Weight(75.0);
-        riskFormModel2.setPlayer_Height(185.0);
-        riskFormModel2.setPrevious_Injuries(3);
-        riskFormModel2.setTraining_Intensity(9);
-        riskFormModel2.setRecovery_Time(4);
+        double expectedHeight = 180.2;
+        riskFormModel2.setPlayer_Height(expectedHeight);
+        assertEquals(expectedHeight, riskFormModel2.getPlayer_Height(), "La altura del jugador no se establece correctamente");
+    }
 
-        assertEquals(30, riskFormModel2.getPlayer_Age());
-        assertEquals(75.0, riskFormModel2.getPlayer_Weight());
-        assertEquals(185.0, riskFormModel2.getPlayer_Height());
-        assertEquals(3, riskFormModel2.getPrevious_Injuries());
-        assertEquals(9, riskFormModel2.getTraining_Intensity());
-        assertEquals(4, riskFormModel2.getRecovery_Time());
+    @Test
+    public void testGetSetPreviousInjuries() {
+        RiskFormModel2 riskFormModel2 = new RiskFormModel2(0, 0, 0, 2, 0, 0);
+
+        int expectedPreviousInjuries = 2;
+        riskFormModel2.setPrevious_Injuries(expectedPreviousInjuries);
+        assertEquals(expectedPreviousInjuries, riskFormModel2.getPrevious_Injuries(), "Las lesiones previas no se establecen correctamente");
+    }
+
+    @Test
+    public void testGetSetTrainingIntensity() {
+        RiskFormModel2 riskFormModel2 = new RiskFormModel2(0, 0, 0, 0, 8, 0);
+
+        int expectedIntensity = 8;
+        riskFormModel2.setTraining_Intensity(expectedIntensity);
+        assertEquals(expectedIntensity, riskFormModel2.getTraining_Intensity(), "La intensidad de entrenamiento no se establece correctamente");
+    }
+
+    @Test
+    public void testGetSetRecoveryTime() {
+        RiskFormModel2 riskFormModel2 = new RiskFormModel2(0, 0, 0, 0, 0, 3);
+
+        int expectedRecoveryTime = 3;
+        riskFormModel2.setRecovery_Time(expectedRecoveryTime);
+        assertEquals(expectedRecoveryTime, riskFormModel2.getRecovery_Time(), "El tiempo de recuperación no se establece correctamente");
     }
 }
