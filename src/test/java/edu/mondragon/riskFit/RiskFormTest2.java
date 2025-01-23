5832bea4ab66417ead416a9bd5f518f7c371a63b
lidealvarez 2 <lide.alvarez2@alumni.mondragon.edu>
2025-01-23 18:08:39 +0100

    coverage igotzen

--- src/test/java/edu/mondragon/riskFit/RiskFormTest2.java
+++ src/test/java/edu/mondragon/riskFit/RiskFormTest2.java
@@ -38,4 +38,23 @@ public class RiskFormTest2 {
         assertEquals(9, riskFormModel2.getTraining_Intensity());
         assertEquals(4, riskFormModel2.getRecovery_Time());
     }
+    
+    @Test
+    public void testSettersAndGettersTogether() {
+        RiskFormModel2 riskFormModel2 = new RiskFormModel2(25, 70.5, 180.2, 2, 8, 3);
+
+        riskFormModel2.setPlayer_Age(30);
+        riskFormModel2.setPlayer_Weight(75.0);
+        riskFormModel2.setPlayer_Height(185.0);
+        riskFormModel2.setPrevious_Injuries(3);
+        riskFormModel2.setTraining_Intensity(9);
+        riskFormModel2.setRecovery_Time(4);
+
+        assertEquals(30, riskFormModel2.getPlayer_Age());
+        assertEquals(75.0, riskFormModel2.getPlayer_Weight());
+        assertEquals(185.0, riskFormModel2.getPlayer_Height());
+        assertEquals(3, riskFormModel2.getPrevious_Injuries());
+        assertEquals(9, riskFormModel2.getTraining_Intensity());
+        assertEquals(4, riskFormModel2.getRecovery_Time());
+    }
 }
